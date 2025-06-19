037dd62b49f9d1124ea203741c903c93ab4499b4
praveen <s.praveen795@gmail.com>
2025-06-19 22:41:58 +0530

    Update\ Charts

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_excel(r'data/Car Sales in India - 2024 (Unpivot Version).xlsx')
 
@@ -7,4 +9,20 @@ print(df.head())
 
 print(df.info())
 
-print(df.summary())
+# Favourite manufacturer
+plt.figure(figsize=[15,4])
+sns.countplot(data=df, x='Make')
+plt.title("Indian's favourite manufacturers")
+plt.show()
+
+# Favourite model
+plt.figure(figsize=[4,15])
+sns.countplot(data=df, y='Model')
+plt.title("Indian's favourite model")
+plt.show()
+
+# Favourite body type
+plt.figure(figsize=[15,4])
+sns.countplot(data=df, x='Body Type')
+plt.title("Indian's favourite body type")
+plt.show()
