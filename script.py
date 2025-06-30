e377b3d447161ba665da80cdf9e7a7f5dee0ffc0
praveen <s.praveen795@gmail.com>
2025-06-30 23:32:25 +0530

    Update annotation\ bt_make1

--- script.py
+++ script.py
@@ -89,13 +89,65 @@ print(df.select_dtypes('number').describe())
 # Bivariate Analysis
 
 ## Product body available from manufacturer
-plt.figure(figsize=[4,15])
-sns.countplot(data=df, y='Make', hue='Body Type')
-plt.title("All manufacturers in India")
+bt_make1 = df.groupby(['Make', 'Body Type'])['Sales'].count().reset_index()
+bt_make1 = pd.pivot_table(
+    bt_make1, 
+    values=['Sales'], 
+    index=['Make'], 
+    columns=['Body Type'], 
+    fill_value=0, 
+    aggfunc="sum"
+    ).reset_index()
+
+print(bt_make1)
+
+bt_make1.columns = ['Make', 'SUV', 'Hatchback', 'Sedan', 'MUV', 'Others']
+bt_make1['Total'] = bt_make1[['SUV', 'Hatchback', 'Sedan', 'MUV', 'Others']].sum(axis=1)
+
+bt_perc1 = bt_make1.select_dtypes('number').div(bt_make1['Total']*0.01, axis=0)
+bt_make1.update(bt_perc1)
+bt_make1.drop('Total', axis=1, inplace=True)
+bt_make1.set_index('Make', inplace=True)
+
+fig5, ax4 = plt.subplots(figsize=[4,15])
+bottom = np.zeros(len(bt_make1))
+
+# Create each bar with values using a loop
+for i, col in enumerate(bt_make1.columns):
+    ax4.barh(bt_make1.index, bt_make1[col], left=bottom, label=col)
+    bottom+=bt_make1[col].values
+
+# Calculate total values for positioning the total value of stacked bar (100)
+totals1 = bt_make1.sum(axis=1)
+x_offset = 4
+
+# Assigning total length of the bar as label (annotation)
+for i, t in enumerate(totals1):
+    ax4.text(t + x_offset, i, round(t), va='center', weight='bold')
+
+# Applying labels (annotation) for the middle bars
+for bar in ax4.patches:
+    width = bar.get_width()
+    if width > 3:  # Only label visible bars
+        ax4.text(
+            bar.get_x() + width / 2,
+            bar.get_y() + bar.get_height() / 2,
+            round(width),
+            ha='center',
+            va='center',
+            color='white',
+            weight='bold',
+            size=8
+        )
+
+# Chart control commands
+plt.xlim(0,200)
+ax4.legend(loc='upper right')
+plt.tight_layout()
+plt.title("Count of car makers and body types in India")
 plt.show()
 
 # Sales by manufacturer and body type
-# bt_make['perc_sales'] = (bt_make['Sales']*100)/bt_make['Sales'].sum()
 bt_make2 = pd.pivot_table(
     df, 
     values=['Sales'], 
