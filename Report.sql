bc93528b4b54cd0cfcbd061074fca5dc60b91c3d
praveen <s.praveen795@gmail.com>
2026-05-08 14:09:50 +0530

    update report\top5 percentage

--- Report.sql
+++ Report.sql
@@ -6,7 +6,10 @@ SELECT body_type, Sales*100/Total AS 'margin' FROM Body_type2024;
 SELECT body_type FROM Body_type2024 ORDER BY Sales DESC LIMIT 1;
 
 # Total unsold units by body type?
-SELECT body_type, (Total - Sales) as unsold_units FROM Body_type2024;
+SELECT body_type, (Total - Sales) AS unsold_units FROM Body_type2024;
 
 # What is the ratio of sales to unsold units for each body type?
-SELECT body_type, Sales/(Total - Sales) as sale_ratio FROM Body_type2024;
+SELECT body_type, Sales/(Total - Sales) AS sale_ratio FROM Body_type2024;
+
+# What is the total percentage of sales contributed by the top 5 OEMs?
+SELECT sum(a.perc_sales) FROM (SELECT Make, perc_sales FROM OEM_data SORT BY DESC LIMIT 5) AS a;
